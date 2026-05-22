221e411659ef5a5968f3c4dc985f8ac9475f2a52
Sagar-Kap <69609200+Sagar-Kap@users.noreply.github.com>
2026-05-22 05:53:53 +0300

    fix: eslint errors

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,11 +2,11 @@ export default defineNuxtConfig({
   modules: ['@weburz/particle-canvas'],
 
   devtools: { enabled: true },
-  compatibilityDate: 'latest',
 
   app: {
     baseURL: process.env.NUXT_APP_BASE_URL || '/',
   },
+  compatibilityDate: 'latest',
 
   nitro: {
     preset: 'github_pages',
